e07fe949af4a8b5710b6e937d5e9b061d8ef658c
Quoc Huy <quochuy270800@gmail.com>
2021-02-12 15:20:18 +0200

    html added

--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,8 @@ const rootDir = require('../util/path')
 const router = express.Router()
 
 router.get('/', (req,res,next) => {
-    // res.sendFile(path.join(rootDir ,'views','shop.html'))
-    res.send('<h1>Hello from express</h1>')
+    res.sendFile(path.join(rootDir ,'views','shop.html'))
+   
 })
 
 module.exports = router
