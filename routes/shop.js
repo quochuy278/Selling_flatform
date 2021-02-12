516164b2a52e619cd90bada14f46a2435ac90122
Quoc Huy <quochuy270800@gmail.com>
2021-02-12 15:15:28 +0200

    Routes added

---
+++ routes/shop.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const express = require('express');
+const rootDir = require('../util/path')
+
+const router = express.Router()
+
+router.get('/', (req,res,next) => {
+    // res.sendFile(path.join(rootDir ,'views','shop.html'))
+    res.send('<h1>Hello from express</h1>')
+})
+
+module.exports = router
